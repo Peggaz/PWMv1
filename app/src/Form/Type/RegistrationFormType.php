c3260e4c08ed0ff0df0e16982c0f7a7ff663066f
Jakub Nowocień <jakub.nowocien@student.uj.pl>
2022-06-28 19:31:52 +0200

    dokuumentacja

--- app/src/Form/Type/RegistrationFormType.php
+++ app/src/Form/Type/RegistrationFormType.php
@@ -34,17 +34,10 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-//            ->add('agreeTerms', CheckboxType::class, [
-//                'mapped' => false,
-//                'constraints' => [
-//                    new IsTrue([
-//                        'message' => 'You should agree to our terms.',
-//                    ]),
-//                ],
-//            ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+                /** instead of being set onto the object directly,
+                 this is read and encoded in the controller
+                 */
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
@@ -54,7 +47,7 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
+                        /** max length allowed by Symfony for security reasons */
                         'max' => 4096,
                     ]),
                 ],
